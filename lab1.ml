5c52e0e1468f6985bd8e6989d811ee8bd35c5508
Nwanneka Okolo <nnekaokolo@Nwannekas-MacBook-Pro.local>
2018-02-01 15:26:06 -0500

    exercise 6

--- lab1.ml
+++ lab1.ml
@@ -177,10 +177,12 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all l =
+  match l with
+    [] -> []
+  | h :: t -> (h * h) :: square_all t;;
 
-let exercise6 = [] ;;
+let exercise6 = square_all [3;4;5] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
